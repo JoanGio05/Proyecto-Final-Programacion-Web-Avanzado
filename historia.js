4c073ed357b83dc0185244fc025cee23f825ceec
JoanGio05 <joan.giocoli2005@gmail.com>
2022-04-13 22:08:25 -0300

    error en la barra del video

--- historia.js
+++ historia.js
@@ -1,9 +1,17 @@
 let botonInicio = document.getElementById('btn1');
 let botonPausa = document.getElementById('btn2');
 let video = document.querySelector('video');
+let p = document.getElementsByClassName('tiempo');
+
 botonInicio.addEventListener('click',()=>{
     video.play();
 })
+
 botonPausa.addEventListener('click',()=>{
     video.pause();
+    p[0].textContent = `Duracion video: ${video.duration}`;
+})
+
+window.addEventListener('load', ()=>{
+    p[0].textContent = `Duracion video: ${video.duration}`;
 })
